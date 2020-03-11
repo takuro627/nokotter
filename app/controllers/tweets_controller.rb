2102f78919d551360773a3ea1b805d10b618fe59
takuro627 <t.i.ryuf14@gmail.com>
2020-03-11 23:11:59 +0900

    add tweets controller actions

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -7,4 +7,31 @@ class TweetsController < ApplicationController
     @tweet = Tweet.new
   end
 
+  def create
+    Tweet.create(tweet_params)
+  end
+
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def update
+    tweet = Tweet.find(params[:id])
+    tweet.update(tweet_params)
+  end
+
+  def show
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def destroy
+    tweet = Tweet.find(params[:id])
+    tweet.destroy
+  end
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:name, :image, :text)
+  end
+
 end
