9e8930e9f308803a5e90378bd525402f492ebffd
takuro627 <t.i.ryuf14@gmail.com>
2020-04-28 21:04:32 +0900

    setting gravatar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def show
     user = User.find(params[:id])
+    @user = User.find(params[:id])
     @nickname = user.nickname
     @tweets = user.tweets.page(params[:page]).per(10).order("created_at DESC")
   end
