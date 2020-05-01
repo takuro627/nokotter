877cfb3bfdb0bcd1203077a0979b0e2440b1679c
takuro627 <t.i.ryuf14@gmail.com>
2020-05-01 14:00:20 +0900

    update application_controller.rb
    ログインログアウト後のリダイレクト先を設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,12 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
   end
+# ログインログアウト後のリダイレクト先
+  def after_sign_in_path_for(resource)
+    tweets_path
+  end
+  
+  def after_sign_out_path_for(resource)
+    tweets_path
+  end
 end
