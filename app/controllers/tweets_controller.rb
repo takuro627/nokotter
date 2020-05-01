a963b83adf11937018042180fadb9ea60f80ae33
takuro627 <t.i.ryuf14@gmail.com>
2020-05-01 13:59:23 +0900

    modify move_to_index

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,6 @@
 class TweetsController < ApplicationController
   before_action :set_tweet, only: [:edit, :show]
-  before_action :move_to_index, except: [:index, :show]
+  before_action :move_to_index, except: [:index, :show, :top]
   before_action :correct_user, only: [:edit, :update]
 
   def index
