99472eee4b6f861592d7620753eab28118765be3
takuro627 <t.i.ryuf14@gmail.com>
2020-05-01 13:59:42 +0900

    update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   root 'tweets#top'
-  resources :tweets
+  resources :tweets do
+    get 'top', to: 'tweets#top'
+  end
   resources :users, only: :show
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
