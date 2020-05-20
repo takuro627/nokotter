36ae7ffc6554eeebe5c96e43087dd63a2d56a766
takuro627 <t.i.ryuf14@gmail.com>
2020-05-20 18:39:06 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,4 +28,11 @@ class User < ApplicationRecord
     following.include?(other_user)
   end
 
+  def feed
+    following_ids = "SELECT followed_id FROM relationships
+                     WHERE follower_id = :user_id"
+    Tweet.where("user_id IN (#{following_ids})
+                     OR user_id = :user_id", user_id: id)
+  end
+
 end
