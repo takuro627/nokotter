49a518c8f6459450b5a762f897a5d02261101225
takuro627 <t.i.ryuf14@gmail.com>
2020-05-23 23:23:30 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_04_055304) do
+ActiveRecord::Schema.define(version: 2020_05_21_095907) do
 
   create_table "relationships", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "follower_id"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2020_05_04_055304) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.integer "in_reply_to"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
