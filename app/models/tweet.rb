70d96d8f5b0d14df3a26f3abf0def984fb7972a0
takuro627 <t.i.ryuf14@gmail.com>
2020-05-24 21:28:47 +0900

    Update tweet.rb
    before _save の設定

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -4,4 +4,12 @@ class Tweet < ApplicationRecord
   belongs_to :user
 
   mount_uploader :image, ImageUploader
+  
+  before_save { in_reply_to = reply_user.to_s }
+  
+  def reply_user
+    if user_unique_name = content.match(/(@[^\s]+)\s.*/)
+      user_unique_name[1]
+    end
+  end
 end
