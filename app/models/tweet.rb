616ac443bae9acb77504bb38136e4193bf9b059b
takuro627 <t.i.ryuf14@gmail.com>
2020-06-01 19:11:38 +0900

    Update tweet.rb

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -7,6 +7,9 @@ class Tweet < ApplicationRecord
   validate :reply_to_user
   belongs_to :user
 
+  has_many :likes, dependent: :destroy
+  has_many :liking_users, through: :likes, source: :user
+
   mount_uploader :image, ImageUploader
   
   def set_in_reply_to
