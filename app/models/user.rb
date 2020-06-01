8c8bf8372d2c24294db14811e819e3cdc676add3
takuro627 <t.i.ryuf14@gmail.com>
2020-06-01 19:11:30 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,9 @@ class User < ApplicationRecord
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
   
+  has_many :likes, dependent: :destroy
+  has_many :like_tweets, through: :likes, source: :tweet
+
   # ユーザーをフォローする
   def follow(other_user)
     following << other_user
