c4cbe488e5ec4626db923bac53257dda7653e2b6
takuro627 <t.i.ryuf14@gmail.com>
2020-06-09 21:21:04 +0900

    Update likes_controller.rb

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -10,7 +10,7 @@ class LikesController < ApplicationController
   def destroy
     @like = Like.find_by(user_id: current_user.id, tweet_id: params[:tweet_id])
     @like.destroy
-    redirect_to redirect_to tweets_path
+    redirect_to tweets_path
   end
 
   private
