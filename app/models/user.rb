1485140cbc74ff2124df7d9bc63ec604aedd0ad1
takuro627 <t.i.ryuf14@gmail.com>
2020-06-23 22:23:52 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -40,10 +40,10 @@ class User < ApplicationRecord
 
   def self.search(search)   
     if search  
-      where(['name LIKE ?', "%#{search}%"])   
+      where(['nickname LIKE ?', "%#{search}%"])   
     else  
       all  
     end  
   end
-    
+
 end
